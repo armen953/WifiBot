65306f09b9cb6353a4fb2166084f9d11fbd3aab9
esirem <esirem@c70007113.ensgt.u-bourgogne.fr>
2018-04-27 16:26:12 +0200

    tcp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QtNetwork>
+
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,7 +18,51 @@ MainWindow::~MainWindow()
 void MainWindow::on_btnConnect_clicked()
 {
 
+    this->mySocket = new QTcpSocket(this);
+
+    connect(mySocket, SIGNAL(connected()),this, SLOT(connected()));
+    connect(mySocket, SIGNAL(disconnected()),this, SLOT(disconnected()));
+    connect(mySocket, SIGNAL(bytesWritten(qint64)),this, SLOT(bytesWritten(qint64)));
+    connect(mySocket, SIGNAL(readyRead()),this, SLOT(readyRead()));
+
+    qDebug() << "connecting...";
+
+    // this is not blocking call
+    mySocket->connectToHost("https://www.google.com", 80);
+
+    // we need to wait...
+    if(!mySocket->waitForConnected(5000))
+    {
+        qDebug() << "Error: " << mySocket->errorString();
+    }
 }
+
+void MainWindow::connected()
+{
+    qDebug() << "connected...";
+
+    // Hey server, tell me about you.
+    mySocket->write("HEAD / HTTP/1.0\r\n\r\n\r\n\r\n");
+}
+
+void MainWindow::disconnected()
+{
+    qDebug() << "disconnected...";
+}
+
+void MainWindow::bytesWritten(qint64 bytes)
+{
+    qDebug() << bytes << " bytes written...";
+}
+
+void MainWindow::readyRead()
+{
+    qDebug() << "reading...";
+
+    // read the data from the socket
+    qDebug() << mySocket->readAll();
+}
+
 
 void MainWindow::on_btnDeconnect_clicked()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 
+#include <QTcpSocket>
+#include <QAbstractSocket>
+#include <QDebug>
+
 namespace Ui {
 class MainWindow;
 }
@@ -19,9 +23,16 @@ private slots:
     void on_btnConnect_clicked();
 
     void on_btnDeconnect_clicked();
+    void bytesWritten(qint64 bytes);
+    void readyRead();
+    void connected();
+    void disconnected();
 
 private:
     Ui::MainWindow *ui;
+    QTcpSocket * mySocket;
+    QString hostname;
+    int port;
 };
 
 #endif // MAINWINDOW_H
